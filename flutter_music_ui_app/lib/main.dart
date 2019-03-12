0d062fc8fd4e1bc3009d787d3dd81ec8027bf3a5
thientv7 <thientvse@gmail.com>
2019-03-12 14:28:15 +0700

    add image and build

--- flutter_music_ui_app/lib/main.dart
+++ flutter_music_ui_app/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:fluttery_seekbar/fluttery_seekbar.dart';
 
 void main() => runApp(MyApp());
 
@@ -59,6 +62,28 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    Widget _buildRadialSeekbar() {
+      double _thumbPercent = 0.4;
+
+      return RadialSeekBar(
+        trackColor: Colors.red.withOpacity(.5),
+        trackWidth: 1.0,
+        progressColor: Color(0xFFFE1483),
+        progressWidth: 2.0,
+        thumbPercent: _thumbPercent,
+        thumb: CircleThumb(
+          color: Color(0xFFFE1483),
+          diameter: 20.0,
+        ),
+        progress: _thumbPercent,
+        onDragUpdate: (double percent) {
+          setState(() {
+            _thumbPercent = percent;
+          });
+        },
+      );
+    }
+
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -66,46 +91,238 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: Color(0xFFFE1483),
+            ),
+            onPressed: () {}),
+        title: Text(
+          "Music World",
+          style: TextStyle(color: Color(0xFFFE1483), fontFamily: "Nexa"),
+        ),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(
+                Icons.menu,
+                color: Color(0xFFFE1483),
+              ),
+              onPressed: () {})
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 5.0,
+          ),
+          Center(
+              child: Container(
+            width: 150.0,
+            height: 150.0,
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                      color: Color(0xFFFE1483).withOpacity(.5),
+                      shape: BoxShape.circle),
+                  child: Padding(
+                    padding: const EdgeInsets.all(3.0),
+                    child: _buildRadialSeekbar(),
+                  ),
+                ),
+                Center(
+                  child: ClipOval(
+                    clipper: MClipper(),
+                    child: Image.asset(
+                      'assets/images/justine.jpg',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )),
+          SizedBox(
+            height: 10.0,
+          ),
+          Column(
+            children: <Widget>[
+              Text(
+                "Justine Bieber fit. Never say",
+                style: TextStyle(
+                    color: Color(0xFFFE1483),
+                    fontSize: 15.0,
+                    fontFamily: "Nexa"),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              Text(
+                "The Weekend",
+                style: TextStyle(
+                    color: Color(0xFFFE1483),
+                    fontSize: 12.0,
+                    fontFamily: "NexaLight"),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          Container(
+            width: 300.0,
+            height: 100.0,
+            child: Stack(
+              children: <Widget>[
+                Center(
+                  child: Container(
+                    height: 50.0,
+                    width: 250.0,
+                    decoration: BoxDecoration(
+                        border:
+                            Border.all(color: Color(0xFFed3961), width: 2.0),
+                        borderRadius: BorderRadius.circular(30.0)),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.fast_rewind,
+                            size: 40.0,
+                            color: Color(0xFFed3961),
+                          ),
+                          Expanded(child: Container()),
+                          Icon(Icons.fast_forward,
+                              size: 40.0, color: Color(0xFFed3961)),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    width: 70.0,
+                    height: 70.0,
+                    decoration: BoxDecoration(
+                        color: Color(0xFFed3961), shape: BoxShape.circle),
+                    child: IconButton(
+                        icon: Icon(
+                          Icons.play_arrow,
+                          size: 25.0,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {}),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          Container(
+            height: 180.0,
+            width: double.infinity,
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                    left: -20,
+                    child: Container(
+                      width: 40.0,
+                      height: 180.0,
+                      decoration: BoxDecoration(
+                          color: Color(0xFFed3961),
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(30.0),
+                              bottomRight: Radius.circular(30.0))),
+                    )),
+                Positioned(
+                    right: -20,
+                    child: Container(
+                      width: 40.0,
+                      height: 180.0,
+                      decoration: BoxDecoration(
+                          color: Color(0xFFed3961),
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(30.0),
+                              bottomLeft: Radius.circular(30.0))),
+                    )),
+                Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      song('assets/images/cover_01.jpg', "Never say",
+                          "Believe 2012"),
+                      song('assets/images/cover_02.jpg', "Beauty...",
+                          "Believe 2012"),
+                      song('assets/images/cover_03.png', "Boyfriend",
+                          "Believe 2012")
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+Widget song(String image, String title, String subtitle) {
+  return Padding(
+    padding: EdgeInsets.all(8.0),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Image.asset(
+          image,
+          width: 40.0,
+          height: 40.0,
+        ),
+        SizedBox(
+          width: 8.0,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              title,
+              style: TextStyle(
+                  color: Color(0xFFFE1483), fontSize: 14.0, fontFamily: "Nexa"),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+              subtitle,
+              style: TextStyle(
+                  color: Color(0xFFFE1483),
+                  fontSize: 12.0,
+                  fontFamily: "NexaLight"),
+            )
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        )
+      ],
+    ),
+  );
+}
+
+class MClipper extends CustomClipper<Rect> {
+  @override
+  Rect getClip(Size size) {
+    return Rect.fromCircle(
+        center: Offset(size.width / 2, size.height / 2),
+        radius: min(size.width, size.height) / 2);
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Rect> oldClipper) {
+    // TODO: implement shouldReclip
+    return true;
   }
 }
